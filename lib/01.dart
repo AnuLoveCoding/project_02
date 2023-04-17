eae5248b6591c174c00fa45a07caa604f1251fba
anuLoveCoading <anusrivas07@gmail.com>
2023-04-17 21:49:46 +0530

    flutter_project

--- lib/01.dart
+++ lib/01.dart
@@ -3,7 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class MyWidget extends StatelessWidget {
-   MyWidget({super.key});
+  MyWidget({super.key});
 
   var name = TextEditingController();
   var address = TextEditingController();
@@ -11,15 +11,22 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.orangeAccent,
       appBar: AppBar(
         title: Text('Dashboard'),
         centerTitle: true,
       ),
       body: Container(
+        margin: EdgeInsets.all(30),
+        padding: EdgeInsets.all(30),
+        alignment: Alignment.center,
+        width: 200.0,
+        height: 200.0,
+        color: Colors.orangeAccent,
         child: Column(
           children: [
             Text(
-              'myge-colle',
+              'my-collge',
               style: TextStyle(
                   color: Colors.amber[600], fontWeight: FontWeight.bold),
             ),
@@ -27,31 +34,46 @@ class MyWidget extends StatelessWidget {
               controller: name,
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(5),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(5),
                 ),
                 hintText: 'Enter Your name',
               ),
             ),
+
+            SizedBox(height: 10,),
+
             TextField(
               controller: address,
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(5),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(5),
                 ),
                 hintText: 'Enter Your address',
               ),
             ),
 
+            SizedBox(
+              height: 20,
+            ),
 
+            ElevatedButton(
+              onPressed: () {
+                print('${name.text}\n${address.text}');
+              },
+              child: Text('Click on me',style: TextStyle(color: Colors.blueGrey),),
+            ),
           ],
+
         ),
+
       ),
+
     );
   }
 }
