017ce910cd2e2da40f419dc61668678693f3a814
anuLoveCoading <anusrivas07@gmail.com>
2023-04-17 20:41:11 +0530

    git commit -m "Flutter-project"

--- lib/01.dart
+++ lib/01.dart
@@ -9,15 +9,16 @@ class project_one extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
         centerTitle: true,
+        ),
+      body: Container(
+
+        child: Column(
+          children: [
+             Text('Name'),
+             Image(image: NetworkImage('https://images.pexels.com/photos/16162419/pexels-photo-16162419.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'),width: 200.0,height: 200.0,),
+          ],
+        ),
       ),
-    body: Container(
-      child: Column(
-        children: [
-              Text('Name'),
-           Image(image: NetworkImage('https://images.pexels.com/photos/16162419/pexels-photo-16162419.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'),),
-        ],
-      ),
-    ),
     );
   }
 }
