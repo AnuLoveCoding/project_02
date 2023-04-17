0a939154a29de5f32db471d36c59222938a82ab1
anuLoveCoading <anusrivas07@gmail.com>
2023-04-17 21:30:35 +0530

    flutter_project

--- lib/01.dart
+++ lib/01.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class project_one extends StatelessWidget {
-   project_one({Key? key}) : super(key: key);
+class MyWidget extends StatelessWidget {
+   MyWidget({super.key});
+
+  var name = TextEditingController();
+  var address = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +14,41 @@ class project_one extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
         centerTitle: true,
-        ),
+      ),
       body: Container(
-
         child: Column(
           children: [
-             Text('Name'),
-             Image(image: NetworkImage('https://images.pexels.com/photos/16162419/pexels-photo-16162419.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'),width: 200.0,height: 200.0,),
+            Text(
+              'myge-colle',
+              style: TextStyle(
+                  color: Colors.amber[600], fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              controller: name,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Enter Your name',
+              ),
+            ),
+            TextField(
+              controller: address,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Enter Your address',
+              ),
+            ),
+
+
           ],
         ),
       ),
