5f7d27ce794c28ae1dc8ec6c360700daf8fdadc7
anuLoveCoading <anusrivas07@gmail.com>
2023-04-17 22:47:09 +0530

    git commit -m "Flutter-project"

--- lib/01.dart
+++ lib/01.dart
@@ -5,6 +5,9 @@ class MyWidget extends StatelessWidget {
 
    var name = TextEditingController();
    var address= TextEditingController();
+   var phone = TextEditingController();
+   var e_mail = TextEditingController();
+   var password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -54,8 +57,61 @@ class MyWidget extends StatelessWidget {
               ),
             ),
           SizedBox(height: 20.0,),
+          TextField(
+            controller: phone,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                icon: Icon(Icons.phone,color: Colors.deepOrangeAccent,shadows: [BoxShadow(offset: Offset(1,2))],),
+                hintText: 'Enter Your Phone number',
+             ),
+          ),
+          SizedBox(height: 20.0,),
+
+          TextField(
+            controller: e_mail,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5)
+              ),
+              icon: Icon(Icons.email,color: Colors.indigo,shadows: [BoxShadow(offset: Offset(1,2))],),
+              hintText: 'Enter Your email'
+            ),
+          ),
+
+          SizedBox(height: 20.0,),
+
+          TextField(
+            controller: password,
+            obscureText: false,
+            decoration: InputDecoration (
+              filled: true,
+              fillColor: Colors.white,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            icon: Icon(Icons.password,color: Colors.indigo,shadows: [BoxShadow(offset: Offset(1,2))],),
+              hintText: 'Enter Your Password',
+            ),
+          ),
+
+          SizedBox(height: 20.0,),
+
           ElevatedButton(onPressed: (){
-            print('${name.text}\n${address.text}');
+            print('${name.text}\n${address.text}\n ${e_mail.text}\n ${phone.text}');
             }, child: Text('Click me!!!'),),
           ],
         ),
